b7570ef0662b3ff75b828e55d5bd963c0069249c
KingSnorlax2 <113091404+KingSnorlax2@users.noreply.github.com>
2023-11-15 09:41:28 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,16 @@
-const MINIMUM = 5;
-const MINIMALNI_VEK = 18; // Přidej minimální věk zde
+const MINIMUM = 15;
+const jmenoFETAKA = window.prompt('Jak si říkáš?', 'Anonym');
+window.alert(`Tomu říkám drsné jméno, ${jmenoFETAKA}!`);
 
-let jmenoFetaka = window.prompt('Wassup feťáku, jaké je tvoje jméno?');
-console.log(`Tvé jméno je ${jmenoFetaka}.`);
-window.alert(`Zdravím tě ${jmenoFetaka}.`);
+const hasNickname = window.confirm('Máš nějakou přezdívku?');
 
-let vekFetaka = parseInt(window.prompt('Kolik ti je let?')); // Změnil jsem načítání věku a převedení na číslo
-console.log(`Tvého věku je ${vekFetaka}.`);
+let prezdivka; // Declare a variable for the nickname
+if (hasNickname) {
+    prezdivka = window.prompt('Jakou máš přezdívku?');
+    const MNOZSTVI = parseInt(window.prompt(`Kolik toho u sebe máš, ${prezdivka}?`), 10);
 
-let mnozstviFetu = window.prompt('Kolik toho u sebe máš?');
-console.log(`Tvé množství fetu je ${mnozstviFetu}.`);
-
-if (vekFetaka < MINIMALNI_VEK) {
-    window.alert('Omlouváme se, ale jsi příliš mladý na tuto konverzaci.');
-} else if (mnozstviFetu == MINIMUM) {
-    window.alert('Vítej mezi námi!');
-} else if (mnozstviFetu > MINIMUM) {
-    window.alert('Proč jsi nepřišel dřív?!');
-} else {
-    window.alert('To je málo, budeš se muset víc snažit!');
+    if (MNOZSTVI < MINIMUM) {
+        window.alert(`Nemáš dostatek`)
+    }
+    else window.alert(`Vítej u nás`)
 }
